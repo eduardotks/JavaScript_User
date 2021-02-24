9408affa9451785a8902a3d8b37bbb1fa15a523b
Eduardo <eduardo@ciadotreinamento.com.br>
2021-02-24 12:43:48 -0300

    Populando tabela com JS

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -3,9 +3,27 @@ var fields = document.querySelectorAll("#form-user-create [name]"); //captura to
 var user = {};
 //-------------------------
 //dataUser = dados do usuário
-function addLine(dataUser)
-{
-    console.log("addLine", dataUser)
+function addLine(dataUser) {
+    var tr = document.createElement("tr");
+
+
+    tr.innerHTML =
+        `
+    <tr>
+    <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+    <td>${dataUser.name}</td>
+    <td>${dataUser.email}</td>
+    <td>${dataUser.admin}</td>
+    <td>${dataUser.birth}</td>
+    <td>
+      <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+      <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+    </td>
+    </tr>
+    `;
+
+    document.getElementById("table-users").appendChild(tr); //acrescenta linha dentro da tabela usando o ID da tabela.
+
 }
 //-------------------------
 document.getElementById("form-user-create").addEventListener("submit", () => {
