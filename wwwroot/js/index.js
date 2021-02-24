e1c62e5a11dacd9f0e5b9ba3786b47615b88d762
Eduardo <eduardo@ciadotreinamento.com.br>
2021-02-24 08:39:41 -0300

    Adaptando uma condição (filtro de gênero)  verificando se sim ou não

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,34 +1,18 @@
-//alert("Hello Word!");
-
-var nome = document.querySelector("#exampleInputName");
-
-nome.value = "TESTANDO";
-nome.style.color = "blue";
-
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked") //para gênero é assim que escolhe o valor selecionado. 
-
-gender.value;
-var birth = document.querySelector("#exampleInputBirth"); //para nascimento
-birth.value;
-var country = document.querySelector("#exampleInputCountry"); //para país
-country.value;
-var email = document.querySelector("#exampleInputEmail"); //para email
-email.value;
-var password = document.querySelector("#exampleInputPassword"); //para senha
-password.value;
-var photo = document.querySelector("#exampleInputFile"); //para arquivo
-photo.value;
-var admin = document.querySelector("#exampleInputAdmin"); //para admin
-admin.value;
-
-
-
+//captura todos os campos do formulário(otimiza a busca)
 var fields = document.querySelectorAll("#form-user-create [name]"); //captura todos os campos que tem name
 
 //para cada campo name executa a função
 fields.forEach(function(field, index){
 
-    console.log(field.id, field.name, field.value, field.checked, field.index);
-
+    if(field.name == "gender")
+    {
+        if(field.checked){
+            console.log("SIM", gender);
+        }
+        //console.log("SIM", field);
+    }
+    else{
+        console.log("NAO");
+    }
 
 });
