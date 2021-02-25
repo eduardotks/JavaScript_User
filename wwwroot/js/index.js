4a0344d27bc4b0d55821e6d2741f71fa098a1e67
Eduardo <eduardo@ciadotreinamento.com.br>
2021-02-25 08:23:30 -0300

    FIX tr JS

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -8,18 +8,18 @@ function addLine(dataUser) {
 
 
     tr.innerHTML =
-        `
-    <tr>
-    <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-    <td>${dataUser.name}</td>
-    <td>${dataUser.email}</td>
-    <td>${dataUser.admin}</td>
-    <td>${dataUser.birth}</td>
-    <td>
-      <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-      <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-    </td>
-    </tr>
+    `
+
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${dataUser.name}</td>
+        <td>${dataUser.email}</td>
+        <td>${dataUser.admin}</td>
+        <td>${dataUser.birth}</td>
+        <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+
     `;
 
     document.getElementById("table-users").appendChild(tr); //acrescenta linha dentro da tabela usando o ID da tabela.
